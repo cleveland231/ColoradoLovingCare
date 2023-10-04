b00b2d026a063f4fc3eeaab1e1d44ca4d950db02
Cleveland Ticoalu <ayocleveland@gmail.com>
2023-10-03 22:19:27 -0600

    styling

--- src/Components/HostHomeProvider/HostHomeProvider.js
+++ src/Components/HostHomeProvider/HostHomeProvider.js
@@ -4,31 +4,36 @@ import './HostHomeProvider.css';
 export const HostHomeProvider = () => {
   return (
     <div className="HostHomeProvider">
-      <p className="Titles"> Becoming a Host Home Provider </p>
-      <p> Step 1. Download and Fill out Both Forms </p>
-      <div classname="DownloadButtonBox">
-        <button classname="DownloadButton"> CLC HHP Application </button>
-        <button classname="DownloadButtons"> Caps Check Form </button>
-      </div>
-      <p> Step 2. Send to colovingcare1@gmail.com </p>
-      <p> Download to View Frequently Asked Questions </p>
-        <button classname="DownloadButton"> Frequently Asked Questions </button>
+      <h2 className="Titles"> Becoming a Host Home Provider </h2>
 
       <div className="MinimumQualifications">
-      <h3> Minimum Qualifications </h3>
-      <li> Prior experience as a Host Home Provider is preferred. </li>
-      <li> Minimum age requirement is 18 years old. </li>
-      <li> Possession of a GED or high school diploma is necessary. </li>
-      <li> Reliable transportation is essential, with the ability to safely transport. </li>
-      <li> Successful completion of multiple background checks is required. </li>
-      <li> Capable of lifting weights ranging from 50 to 75 pounds. </li>
-      <li> Excellent interpersonal skills to engage with families and individuals who have developmental disabilities. </li>
-      <li> Openness to working with individuals who exhibit challenging behavior. </li>
-      <li> Strong attention to detail and task-oriented mindset. </li>
-      <li> Reliability, dependability, and self-motivation are key. </li>
-      <li> An appropriate living environment with at least one available bedroom is necessary. </li>
-      <li> Prior experience and knowledge of working with the developmentally disabled population. </li>
+
+      <h3> Step 1. Download and Fill out Both Forms </h3>
+      <h3> Step 2. Send to colovingcare1@gmail.com </h3>
+      <div classname="DownloadButtonBox">
+        <button classname="DownloadButtons"> CLC HHP Application </button>
+        <button classname="DownloadButtons"> Caps Check Form </button>
       </div>
+      <p> Download to View Frequently Asked Questions </p>
+        <button classname="DownloadButtons"> Frequently Asked Questions </button>
+        <h2> Minimum Qualifications </h2>
+          <div className="LiStyling">
+            <li> Prior experience as a Host Home Provider is preferred. </li>
+            <li> Minimum age requirement is 18 years old. </li>
+            <li> Possession of a GED or high school diploma is necessary. </li>
+            <li> Reliable transportation is essential, with the ability to safely transport. </li>
+            <li> Successful completion of multiple background checks is required. </li>
+            <li> Capable of lifting weights ranging from 50 to 75 pounds. </li>
+            <li> Excellent interpersonal skills to engage with families and clients who have developmental disabilities. </li>
+            <li> Openness to working with individuals who exhibit challenging behavior. </li>
+            <li> Strong attention to detail and task-oriented mindset. </li>
+            <li> Reliability, dependability, and self-motivation are key. </li>
+            <li> An appropriate living environment with at least one available bedroom is necessary. </li>
+            <li> Prior experience and knowledge of working with the developmentally disabled population. </li>
+          </div>
+        </div>
+
+
     </div>
   );
 }
