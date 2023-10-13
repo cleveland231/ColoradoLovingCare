4ce252ef5aa84b281c9e227bad1b1b2b719b8f19
Cleveland Ticoalu <ayocleveland@gmail.com>
2023-10-13 09:46:54 -0600

    Button color change

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,8 +6,7 @@ export const Footer = () => {
     <div className="Footer">
       <div className="FooterWords"> Phone: 1.888.249.1444 </div>
       <div className="FooterWords"> Email:  colovingcare1@gmail.com </div>
-      <div className="FooterWords"> Monday - Friday: 9:00am - 5:00pm </div>
-      <div className="FooterWords"> Address: 1776 S. Jackson St. Denver, CO 80210 #405 </div>
+      <div className="FooterWords"> Mailing Address: 1776 S. Jackson St. Denver, CO 80210 #405 </div>
     </div>
   );
 }
